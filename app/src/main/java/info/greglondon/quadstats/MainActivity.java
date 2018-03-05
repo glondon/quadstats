2693b6e53a8d1ccdfd1cff4fb26f263cd43f3e36
Greg London <gregdlondon@gmail.com>
2018-03-04 20:10:39 -0600

    #2 updated button id

--- app/src/main/java/info/greglondon/quadstats/MainActivity.java
+++ app/src/main/java/info/greglondon/quadstats/MainActivity.java
@@ -10,7 +10,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        final Button addButton = findViewById(R.id.button_id);
+        final Button addButton = findViewById(R.id.btnSubmit);
         addButton.setOnClickListener(new View.OnClickListener(){
             public void onClick(View v){
                 //TODO validate and process items
