0935deb1b5f5ef669dc17591495056923cc3ed6d
glondon <gregdlondon@gmail.com>
2018-03-04 20:33:43 -0600

    #2 retrieved value of txtAmount

--- app/src/main/java/info/greglondon/quadstats/MainActivity.java
+++ app/src/main/java/info/greglondon/quadstats/MainActivity.java
@@ -2,6 +2,10 @@ package info.greglondon.quadstats;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,10 +14,13 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        final EditText txtAmount = (EditText)findViewById(R.id.txtInput);
         final Button addButton = findViewById(R.id.btnSubmit);
+
         addButton.setOnClickListener(new View.OnClickListener(){
             public void onClick(View v){
                 //TODO validate and process items
+                Log.v("EditText", txtAmount.getText().toString());
             }
         });
     }
