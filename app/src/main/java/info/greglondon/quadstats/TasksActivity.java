1796794280e9d4e2a160f4a5e91e59c969eed0c6
glondon <gregdlondon@gmail.com>
2019-07-07 20:17:00 -0500

    GH-36 updated TaskActivity class to use CustomAdapter

--- app/src/main/java/info/greglondon/quadstats/TasksActivity.java
+++ app/src/main/java/info/greglondon/quadstats/TasksActivity.java
@@ -13,41 +13,30 @@ import android.widget.ListView;
 import android.widget.Toast;
 import java.util.ArrayList;
 
-public class TasksActivity extends AppCompatActivity {
+import android.app.ListActivity;
+
+public class TasksActivity extends ListActivity {
 
     DBHandler db;
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_tasks);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+    public void onCreate(Bundle icicle) {
+        super.onCreate(icicle);
+        CustomAdapter adapter = new CustomAdapter(this, generateData());
+        setListAdapter(adapter);
+    }
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
-
-        ListView listView = (ListView)findViewById(R.id.taskList);
+    private ArrayList<Task> generateData(){
+        ArrayList<Task> tasks = new ArrayList<Task>();
         db = new DBHandler(this,null,null,1);
-        ArrayList<String> tasks = new ArrayList<>();
         Cursor data = db.getAllTasks();
         if(data.getCount() == 0)
             Toast.makeText(TasksActivity.this, "No tasks found", Toast.LENGTH_SHORT).show();
         else{
-            while(data.moveToNext()){
-                tasks.add(data.getString(1));
-                tasks.add(Integer.toString(data.getInt(0)));
-                tasks.add(data.getString(2));
-                ListAdapter listAdapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, tasks);
-                listView.setAdapter(listAdapter);
-            }
+            while(data.moveToNext())
+                tasks.add(new Task(data.getInt(0), data.getString(1), data.getString(2)));
         }
+
+        return tasks;
     }
 
 }
